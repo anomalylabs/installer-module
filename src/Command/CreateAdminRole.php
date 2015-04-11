155cd1adff2ed9b28f37705781b3a964991aa90b
Ryan Thompson <service@aiwebsystems.com>
2015-04-10 22:28:24 -0500

    Fix

--- src/Command/CreateAdminRole.php
+++ src/Command/CreateAdminRole.php
@@ -25,7 +25,7 @@ class CreateAdminRole implements SelfHandling
      */
     public function handle(RoleRepositoryInterface $roles, UserRepositoryInterface $users, UserManager $manager)
     {
-        $user = $users->findUserByUsername('admin');
+        $user = $users->findUserByUsername(env('ADMIN_USERNAME'));
 
         if (!$role = $roles->findBySlug('admin')) {
             $role = $roles->create(['en' => ['name' => 'Admin'], 'slug' => 'admin']);
