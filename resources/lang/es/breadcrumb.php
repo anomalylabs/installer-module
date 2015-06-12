0eceb1845b1ae26d3121ac0cf83f4531238167a5
Jose Luis Fonseca <jose@ditecnologia.com>
2015-06-12 08:40:00 -0500

    Spanish Translation

---
+++ resources/lang/es/breadcrumb.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'installer' => 'Instalador'
+];
---
+++ resources/lang/es/field.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+    'license'               => [
+        'label'        => '¿Está de acuerdo con los términos de esta licencia y de las licencias de todos los complementos?',
+        'instructions' => 'Por favor, tenga en cuenta que todos los complementos se pueden licenciar por separado.'
+    ],
+    'database_driver'       => [
+        'label'            => 'Driver',
+        'instructions'     => 'Qué controlador de base de datos le gustaría usar?',
+        'invalid_database' => 'No se pudo conectar con la base de datos.',
+    ],
+    'database_host'         => [
+        'label'        => 'Host',
+        'placeholder'  => 'localhost',
+        'instructions' => 'Cual es el servidor de su base de datos?',
+    ],
+    'database_name'         => [
+        'label'        => 'Base de datos',
+        'placeholder'  => 'streams',
+        'instructions' => 'Cual es el nombre de su base de datos?',
+    ],
+    'database_username'     => [
+        'label'        => 'Nombre de usuario',
+        'placeholder'  => 'root',
+        'instructions' => 'Ingrese el nombre de usuario para su base de datos.',
+    ],
+    'database_password'     => [
+        'label'        => 'Contraseña',
+        'placeholder'  => 'Su contraseña segura',
+        'instructions' => 'Ingrese la contraseña para la conexión con la base de datos.',
+    ],
+    'admin_username'        => [
+        'label'        => 'Nombre de usuario',
+        'placeholder'  => 'admin',
+        'instructions' => 'Cual será el nombre de usuario para el administrador?',
+    ],
+    'admin_email'           => [
+        'label'        => 'Correo electrónico',
+        'placeholder'  => 'example@domain.com',
+        'instructions' => 'Cual será el correo eletrónico para el administrador?',
+    ],
+    'admin_password'        => [
+        'label'        => 'Contraseña',
+        'placeholder'  => 'Ingrese una contraseña segura',
+        'instructions' => 'Cual será la contraseña del administrador?',
+    ],
+    'application_name'      => [
+        'label'        => 'Nombre de la aplicación',
+        'placeholder'  => 'Streams',
+        'instructions' => 'Cual será el nombre de la aplicación?',
+    ],
+    'application_reference' => [
+        'label'        => 'Referencia de la aplicación',
+        'placeholder'  => 'default',
+        'instructions' => 'Cual es la referencia de la aplicación? se usará como prefijo en la base de datos.?',
+    ],
+    'application_domain'    => [
+        'label'        => 'Dominio primario',
+        'placeholder'  => 'localhost',
+        'instructions' => 'Cual será el dominio primario de la aplicación?',
+    ],
+    'application_locale'    => [
+        'label'        => 'Idioma',
+        'instructions' => 'Cual es el idioma por defecto de la aplicación?',
+    ],
+    'application_timezone'  => [
+        'label'        => 'Zona Horaria',
+        'instructions' => 'Cual es la zona horaria de la aplicación?',
+    ],
+];
