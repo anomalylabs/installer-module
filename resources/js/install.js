16843deed1b3284f4d3610548f59e00ef498c110
Ryan Thompson <service@aiwebsystems.com>
2015-06-16 02:51:34 -0500

    Better error handling

--- resources/js/install.js
+++ resources/js/install.js
@@ -2,8 +2,9 @@ var install = function () {
 
     var step = $('body').find('[data-step]').first();
     var progress = $('.progress-bar');
+    var label = $('#label');
 
-    $('#label').text(step.data('step') + '...');
+    label.text(step.data('step') + '...');
 
     $.ajax({
         url: step.data('action'),
@@ -20,7 +21,7 @@ var install = function () {
 
                 progress.width('100%');
 
-                $('#label').text('Ready.');
+                label.text('Ready.');
 
                 setTimeout(function () {
                     $('#finished').removeClass('hidden');
@@ -28,7 +29,8 @@ var install = function () {
             }
         },
         error: function () {
-            progress.addClass('error').find('.label').text('Error');
+            progress.addClass('progress-bar-danger');
+            label.addClass('text-danger').text('There was an error. Please check your error logs.');
         }
     });
 };
