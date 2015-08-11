a86486171e66fde57e0a36d38dfc2e0e309e3927
Ryan Thompson <service@aiwebsystems.com>
2015-08-11 04:09:58 -0500

    Protection

--- src/InstallerModuleServiceProvider.php
+++ src/InstallerModuleServiceProvider.php
@@ -16,21 +16,6 @@ class InstallerModuleServiceProvider extends AddonServiceProvider
 {
 
     /**
-     * The addon routes.
-     *
-     * @var array
-     */
-    protected $routes = [
-        'installer'                       => 'Anomaly\InstallerModule\Http\Controller\InstallerController@index',
-        'installer/install'               => 'Anomaly\InstallerModule\Http\Controller\InstallerController@install',
-        'installer/complete'              => 'Anomaly\InstallerModule\Http\Controller\InstallerController@complete',
-        'installer/command/{command}'     => 'Anomaly\InstallerModule\Http\Controller\InstallerController@command',
-        'installer/module/{module}'       => 'Anomaly\InstallerModule\Http\Controller\InstallerController@module',
-        'installer/seed/{module}'         => 'Anomaly\InstallerModule\Http\Controller\InstallerController@seed',
-        'installer/extension/{extension}' => 'Anomaly\InstallerModule\Http\Controller\InstallerController@extension'
-    ];
-
-    /**
      * Register the service provider.
      */
     public function register()
@@ -39,5 +24,27 @@ class InstallerModuleServiceProvider extends AddonServiceProvider
         $this->dispatch(new CheckDirectoryPermissions());
 
         parent::register();
+    }
+
+    /**
+     * Only return routes if not installed.
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        if (env('INSTALLED')) {
+            return [];
+        } else {
+            return [
+                'installer'                       => 'Anomaly\InstallerModule\Http\Controller\InstallerController@index',
+                'installer/install'               => 'Anomaly\InstallerModule\Http\Controller\InstallerController@install',
+                'installer/complete'              => 'Anomaly\InstallerModule\Http\Controller\InstallerController@complete',
+                'installer/command/{command}'     => 'Anomaly\InstallerModule\Http\Controller\InstallerController@command',
+                'installer/module/{module}'       => 'Anomaly\InstallerModule\Http\Controller\InstallerController@module',
+                'installer/seed/{module}'         => 'Anomaly\InstallerModule\Http\Controller\InstallerController@seed',
+                'installer/extension/{extension}' => 'Anomaly\InstallerModule\Http\Controller\InstallerController@extension'
+            ];
+        }
     }
 }
