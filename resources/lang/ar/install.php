55c356ce32452ed1a27f28eb5f3f776e0dba8bcd
aladinfajr <aladinfajr@gmail.com>
2016-09-10 13:09:10 +0300

    translate installer module to arabic

---
+++ resources/lang/ar/install.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'installing'              => 'جاري التثبيت',
+    'clear_cache'             => 'تنظيف المخزونات',
+    'base_tables'             => 'تثبيت فاعد الجداول',
+    'application'             => 'إنشاء التطبيق',
+    'module'                  => 'تثبيت وحدة :name',
+    'extension'               => 'تثبيت التركيب :name',
+    'seed'                    => 'إضافة بيانات الجداول لـ  :name ',
+    'update_environment_file' => 'تعديل ملف .env',
+    'create_admin_user'       => 'إنشاء المستخدم المدير',
+    'create_admin_role'       => 'إنشاء دور المدير',
+    'create_user_role'        => '‘إنشاء دور المستخدم’',
+    'cleaning_up'             => 'جاري التنظيف',
+];
