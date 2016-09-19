5b957b14f9fe9fbe87bd42f6fddf7757d06c8d8c
Ryan Thompson <ryan@pyrocms.com>
2016-09-19 10:12:16 -0500

    Fix issue where spaces in app names were breaking things

--- src/InstallerModuleInstaller.php
+++ src/InstallerModuleInstaller.php
@@ -35,7 +35,7 @@ class InstallerModuleInstaller
         $data->put('DB_DATABASE', $parameters['database_name']);
         $data->put('DB_USERNAME', $parameters['database_username']);
         $data->put('DB_PASSWORD', $parameters['database_password']);
-        $data->put('APPLICATION_NAME', $parameters['application_name']);
+        $data->put('APPLICATION_NAME', '"' . $parameters['application_name'] . '"');
         $data->put('APPLICATION_DOMAIN', $parameters['application_domain']);
         $data->put('APPLICATION_REFERENCE', $parameters['application_reference']);
         $data->put('DEFAULT_LOCALE', $parameters['application_locale']);
