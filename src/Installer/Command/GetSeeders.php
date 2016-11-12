db59d455c59eaf4abc63777edd27786592c4856c
Ryan Thompson <service@aiwebsystems.com>
2016-11-12 02:10:49 -0600

    Install the default application during installation

--- src/Installer/Command/GetSeeders.php
+++ src/Installer/Command/GetSeeders.php
@@ -1,19 +1,19 @@
 <?php namespace Anomaly\InstallerModule\Installer\Command;
 
+use Anomaly\Streams\Platform\Application\ApplicationRepository;
 use Anomaly\Streams\Platform\Installer\Console\Command\LoadExtensionSeeders;
 use Anomaly\Streams\Platform\Installer\Console\Command\LoadModuleSeeders;
 use Anomaly\Streams\Platform\Installer\Installer;
 use Anomaly\Streams\Platform\Installer\InstallerCollection;
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
 /**
  * Class GetSeeders
  *
- * @link          http://pyrocms.com/
- * @author        PyroCMS, Inc. <support@pyrocms.com>
- * @author        Ryan Thompson <ryan@pyrocms.com>
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
  */
 class GetSeeders
 {
@@ -31,6 +31,22 @@ class GetSeeders
 
         $this->dispatch(new LoadModuleSeeders($installers));
         $this->dispatch(new LoadExtensionSeeders($installers));
+
+        $installers->add(
+            new Installer(
+                'streams::installer.running_seeds',
+                function (ApplicationRepository $applications) {
+                    $applications->create(
+                        [
+                            'name'      => env('APPLICATION_NAME'),
+                            'reference' => env('APPLICATION_REFERENCE'),
+                            'domain'    => env('APPLICATION_DOMAIN'),
+                            'enabled'   => true,
+                        ]
+                    );
+                }
+            )
+        );
 
         $installers->add(
             new Installer(
